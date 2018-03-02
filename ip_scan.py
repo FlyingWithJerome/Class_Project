828bf4f0afc0437deea607d3e4c78a48d7ab6244
FlyingWithJerome <jiarunm@uci.edu>
2018-03-02 14:39:40 -0500

    TODO: encode

--- ip_scan.py
+++ ip_scan.py
@@ -1,6 +1,7 @@
 import socket
 import ipaddress
 import subprocess
+import struct
 import sys
 
 SKIP_LIST = [int(ipaddress.IPv4Address("0.0.0.0")),
@@ -13,17 +14,47 @@ def make_dns_packet():
 	make a customize udp packet (for dns query, carries a message to
 	system admins)
 	'''
-	pass
+	message = "For a Research @Case Western Reserve, Plz Contact jxm959@case.edu"
+	message = bytes(message, "ASCII")
 
-def make_datagram_socket() -> socket.socket:
-	'''
-	make a udp socket with our options. 
-	'''
-	pass
 
-def send_packet(packet, ip_address:ip_address.IPv4Address) -> None:
+	transaction_id = 0xffff # 2 byte short
+	control 	   = 0x0100
+	q_counts       = 0x0001
+	ans_counts     = 0x0000
+	auth_counts    = 0x0000
+	add_counts     = 0x0000 # 2 byte short
+	query          = 0x0c
+	type_          = 0x0001 # 2 byte short (A)
+	class_         = 0x0001 # 2 byte short (IN)
+
+	query_website  = bytes("case.edu", "ASCII")
+
+	format_= "!hh4h"
+
+
+
+
+
+def make_datagram_sockets() -> (socket.socket, socket.socket):
 	'''
-	send a dns packet to the server and wait for response.
+	make a pair of udp socket with our options
+	1. send dns queries
+	2. listen responses. 
+	'''
+	send_socket_instance = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) # Produce a UDP DGRAM socket
+	recv_socket_instance = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) # Produce a UDP DGRAM socket
+	# send_socket_instance.setsockopt(socket.IPPROTO_IP, socket.IP_TTL,     255) # Set the TTL to 255
+
+	return send_socket_instance, recv_socket_instance
+
+
+
+
+def listen_and_check_response(in_socket: socket.socket, out_socket: socket.socket, packet, ip_address:ip_address.IPv4Address) -> bool:
+	'''
+	listen to the server side response and check whether it is a legal &
+	valid response
 	'''
 	pass
 
