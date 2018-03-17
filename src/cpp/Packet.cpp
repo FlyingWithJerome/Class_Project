ce73c516927f96e04474e0ae8c656ee82969b3ab
Frank-MI <miyu3310639@126.com>
2018-03-17 16:09:47 -0400

    Packet pack operation

--- src/cpp/Packet.cpp
+++ src/cpp/Packet.cpp
@@ -1,2 +1,113 @@
 #include "Packet.h"
 
+Packet::Packet()
+{
+	memset(packetData,0,sizeof(packetData));
+	memset(questionAddress,0,sizeof(questionAddress));
+	memset(serverIP,0,sizeof(serverIP));
+	Query	= 0;
+	OpCode	= 0;// Standard Query
+	AA		= 0;
+	TC		= 0;
+	RD		= 1;// Desired to be quried recursively
+	RA		= 0;
+	Z		= 0;// Reserved
+	AD		= 0;
+	CD		= 0;
+	Rcode	= 0;
+}
+
+Packet::Packet(char * inputString)
+{
+	memcpy(packetData,inputString,strlen(inputString));
+	memset(questionAddress,0,sizeof(questionAddress));
+	memset(serverIP,0,sizeof(serverIP));
+}
+
+int Packet::setQuestion(char * input)
+{
+	int len = 0;
+	if(len = strlen(input))
+	{
+		memcpy(questionAddress,input,len)
+		questionCount ++;
+	}
+	return len;
+}
+
+int Packet::setServer(char * input)
+{
+	int len = 0;
+	if(len = strlen(input))
+	{
+		memcpy(serverIP,input,len)
+		serverCount ++;
+	}
+	return len;
+}
+
+char* Packet::pack()
+{
+	char * currentPos = packetData;
+	std::srand(std::time(nullptr));
+	int randomVariable = std::rand();
+	int transactionID = randomVariable % 0xffff;// Randomly select transaction ID;
+
+	//Write in transaction ID
+	*(currentPos++) = transactionID / 0xff;
+	*(currentPos++) = transactionID % 0xff;
+
+	//Write in Flags
+	*(currentPos++) = (QR<<7)|(OpCode<<3)|(AA<<2)|(TC<<1)|(RD);
+	*(currentPos++) = (RA<<7)|(Z<<6)|(AD<<5)|(CD<<4)|(Rcode);
+
+	//Write in Question count
+	*(currentPos++) = 0;
+	*(currentPos++) = questionCount;
+
+	//Write in Answer RR
+	*(currentPos++) = 0;
+	*(currentPos++) = 0;
+
+	//Write in Authority RR
+	*(currentPos++) = 0;
+	*(currentPos++) = 0;
+
+	//Write in Additional RR
+	*(currentPos++) = 0;
+	*(currentPos++) = 0;
+
+	//Write in Name to query
+	*(currentPos) = '.';
+	int addressLength = strlen(questionAddress);
+	memcpy(currentPos+1,questionAddress,addressLength)
+
+	int count = 0;
+	int lastPos = 0;
+	for(int i = 0;i<=addressLength;i++)
+	{
+		if(currentPos[i]=='.'||currentPos==0)
+			if(count)
+			{
+				currentPos[lastPos] = count;
+				count = 0;
+				lastPos = i;
+			}
+			else
+				lastPos = i;
+		else
+			count ++;
+	}
+
+	currentPos += addressLength;
+
+	//Write in Type A
+	*(currentPos++) = 0;
+	*(currentPos++) = 1;
+
+	//Write in Class IN
+	*(currentPos++) = 0;
+	*(currentPos++) = 1;
+
+	return packetData;
+}
--- src/cpp/Packet.h
+++ src/cpp/Packet.h
@@ -1,5 +1,9 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <ctime>
 
 #ifndef _DNS_SCAN_PACKET
 #define _DNS_SCAN_PACKET
@@ -9,13 +13,29 @@
 class Packet
 {
 public:
-	Packet(char* IPAddress=DEFAULT_QUERY_DNS, char* queryAddress=DEFAULT_QUERY_ADDRESS);
+	Packet();
 	Packet(char* inputString);
-	setQuestion(char *);	// Set the question web address
-	setServer(char *);		// Set the server to ask for DNS response
+	int setQuestion(char *);				// Set the question web address
+	int setServer(char *);					// Set the server to ask for DNS response
+	std::vector<std::string> getAnswerIP();	// Get the answer section of IP
+	char * pack();							// Get the packet to be sent
 
 private:
+	char[1000]	packetData;
+	char[100]	questionAddress;
+	int			questionCount;
+	char[20]	serverIP;
+	int			serverCount;
 
-
+	bool	QR;		// 0 for query, 1 for response
+	int		OpCode;	// operation code, see RFC 1035
+	bool	AA;		// Authoritative Answer, 0 is not, 1 is authoritative
+	bool	TC;		// Truncated, 0 is not truncated, 1 is truncated
+	bool	RD;		// Recursion desired, 0 is not desired
+	bool	RA;		// Recursion available, 0 is not available
+	bool	Z;		//---
+	bool	AD;		// Authencated data
+	bool	CD;		// Checking disabled
+	int		Rcode;	// Return code
 };
 #endif
