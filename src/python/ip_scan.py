c51fd184fd111e28743e675845f2df8e00d0d87e
Frank-MI <miyu3310639@126.com>
2018-03-19 14:54:05 -0400

    half of unpack

--- src/python/ip_scan.py
+++ src/python/ip_scan.py
@@ -21,86 +21,86 @@ from email.mime.text import MIMEText
 from ip_scan_query import MultiprocessQuery
 
 def parse_arguments():
-    '''
-    This function helps to parse argument
-    from the command line
-    '''
-    parser = argparse.ArgumentParser(description='Argument Parser for DNS Scanner')
-    parser.add_argument("Start IP Address", type=str, nargs=1, 
-                        help="The leftmost bound for the scanning range (smallest)")
+	'''
+	This function helps to parse argument
+	from the command line
+	'''
+	parser = argparse.ArgumentParser(description='Argument Parser for DNS Scanner')
+	parser.add_argument("Start IP Address", type=str, nargs=1, 
+						help="The leftmost bound for the scanning range (smallest)")
 
-    parser.add_argument("End IP Address", type=str, nargs=1, 
-                        help="The rightmost bound for the scanning range (largest)")
+	parser.add_argument("End IP Address", type=str, nargs=1, 
+						help="The rightmost bound for the scanning range (largest)")
 
-    parser.add_argument("Number of Processes", type=int, nargs="?", default=4, 
-                        help="The number of concurrently working processes, default to be 4")
+	parser.add_argument("Number of Processes", type=int, nargs="?", default=4, 
+						help="The number of concurrently working processes, default to be 4")
 
-    parser.add_argument("TCP Connection", type=str, nargs="?", default="notcp", 
-                        help="Whether to connect with TCP")
+	parser.add_argument("TCP Connection", type=str, nargs="?", default="notcp", 
+						help="Whether to connect with TCP")
 
-    parser.add_argument("Trace Switch", type=str, nargs="?", default="notrace", 
-                        help="Whether to resolve from root")
+	parser.add_argument("Trace Switch", type=str, nargs="?", default="notrace", 
+						help="Whether to resolve from root")
 
-    parser.add_argument("Email Switch", type=str, nargs="?", default="email", 
-                        help="Whether to send email notice")
+	parser.add_argument("Email Switch", type=str, nargs="?", default="email", 
+						help="Whether to send email notice")
 
-    return parser.parse_args()
+	return parser.parse_args()
 
 
 def send_mail_notice():
 
-    with open("mail_notice.txt") as configure:
-        try:
-            [user, passwd, receiver] = configure.readlines()
-        except:
-            return
+	with open("mail_notice.txt") as configure:
+		try:
+			[user, passwd, receiver] = configure.readlines()
+		except:
+			return
 
-    message = [
-        "Subject: =?utf-8?b?U2VucGFpLCDjgbLjgajjgaTjgYrjga3jgYzjgYTjgZTjgajjgYzjgYLjgovjgpPjgafjgZk=?=",
-        "From: {}".format(user),
-        "Scanning Finished! Check your error message or csv output"
-    ]
+	message = [
+		"Subject: =?utf-8?b?U2VucGFpLCDjgbLjgajjgaTjgYrjga3jgYzjgYTjgZTjgajjgYzjgYLjgovjgpPjgafjgZk=?=",
+		"From: {}".format(user),
+		"Scanning Finished! Check your error message or csv output"
+	]
 
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login(user, passwd)
-    server.sendmail(user, receiver, "\r\n".join(message))
-    server.quit()
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.ehlo()
+	server.starttls()
+	server.login(user, passwd)
+	server.sendmail(user, receiver, "\r\n".join(message))
+	server.quit()
 
 def main_entry():
-    '''
-    The entry of the scanner
-    '''
+	'''
+	The entry of the scanner
+	'''
 
-    options = parse_arguments()
+	options = parse_arguments()
 
-    start_ip = getattr(options, "Start IP Address")
-    end_ip   = getattr(options, "End IP Address")
+	start_ip = getattr(options, "Start IP Address")
+	end_ip   = getattr(options, "End IP Address")
 
-    tcp_switch   = getattr(options, "TCP Connection").startswith("tcp")
-    trace_switch = getattr(options, "Trace Switch").startswith("trace")
-    email_switch = getattr(options, "Email Switch").startswith("email")
-    process_num  = getattr(options, "Number of Processes")
+	tcp_switch   = getattr(options, "TCP Connection").startswith("tcp")
+	trace_switch = getattr(options, "Trace Switch").startswith("trace")
+	email_switch = getattr(options, "Email Switch").startswith("email")
+	process_num  = getattr(options, "Number of Processes")
 
-    if email_switch:
-        atexit.register(send_mail_notice)
+	if email_switch:
+		atexit.register(send_mail_notice)
 
-    args = \
-    {
-        "start_ip"   : start_ip[0],
-        "end_ip"     : end_ip[0],
-        "tcp"        : tcp_switch,
-        "trace"      : trace_switch,
-        "process_num": process_num
-    }
+	args = \
+	{
+		"start_ip"   : start_ip[0],
+		"end_ip"     : end_ip[0],
+		"tcp"        : tcp_switch,
+		"trace"      : trace_switch,
+		"process_num": process_num
+	}
 
-    time_a = time.time()
+	time_a = time.time()
 
-    query_object = MultiprocessQuery(**args)
-    query_object.run()
+	query_object = MultiprocessQuery(**args)
+	query_object.run()
 
-    print("Time Elapsed: %6.2f seconds"%(time.time() - time_a))
+	print("Time Elapsed: %6.2f seconds"%(time.time() - time_a))
 
 if __name__ == "__main__":
-    main_entry()
+	main_entry()
--- src/python/ip_scan_result.py
+++ src/python/ip_scan_result.py
@@ -9,44 +9,44 @@ import csv
 
 class Result(object):
 
-    def __init__(self, output_file=None):
+	def __init__(self, output_file=None):
 
-        self.output_file = output_file if output_file else "dns_output.csv"
-        self.result_list = []
+		self.output_file = output_file if output_file else "dns_output.csv"
+		self.result_list = []
 
-    def append_result(self, list_of_result:list) -> None:
-        '''
-        append results to the output list, waiting to be flushed
-        '''
-        self.result_list.append(
-            {
-                "IP address"    : list_of_result[0],
-                "TCP Connection": list_of_result[1].startswith("tcp"),
-                "Trace"         : list_of_result[2].startswith("trace"),
-                "Result"        : list_of_result[3]
-            }
-        )
+	def append_result(self, list_of_result:list) -> None:
+		'''
+		append results to the output list, waiting to be flushed
+		'''
+		self.result_list.append(
+			{
+				"IP address"    : list_of_result[0],
+				"TCP Connection": list_of_result[1].startswith("tcp"),
+				"Trace"         : list_of_result[2].startswith("trace"),
+				"Result"        : list_of_result[3]
+			}
+		)
 
-    def __add__(self, another):
-        assert isinstance(another, Result), "both of them should be Result object"
-        self.result_list += another.result_list
+	def __add__(self, another):
+		assert isinstance(another, Result), "both of them should be Result object"
+		self.result_list += another.result_list
 
-        return self
+		return self
 
-    def __iadd__(self, another):
-        assert isinstance(another, Result), "both of them should be Result object" 
-        self.result_list += another.result_list
+	def __iadd__(self, another):
+		assert isinstance(another, Result), "both of them should be Result object" 
+		self.result_list += another.result_list
 
-        return self
+		return self
 
-    def flush(self) -> None:
-        '''
-        flush the output to the csv file
-        '''
-        header = ["IP address", "TCP Connection", "Trace", "Result"]
-   
-        with open(self.output_file, "w") as out:
-            self.output_csv = csv.DictWriter(out, fieldnames=header)
-            self.output_csv.writeheader()
-            self.output_csv.writerows(self.result_list)
+	def flush(self) -> None:
+		'''
+		flush the output to the csv file
+		'''
+		header = ["IP address", "TCP Connection", "Trace", "Result"]
+
+		with open(self.output_file, "w") as out:
+			self.output_csv = csv.DictWriter(out, fieldnames=header)
+			self.output_csv.writeheader()
+			self.output_csv.writerows(self.result_list)
 
