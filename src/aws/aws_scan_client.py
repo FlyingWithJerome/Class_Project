691345b21bcbb1f70c5b01834eb89e859ebdd534
Frank-MI <miyu3310639@126.com>
2018-04-18 10:57:31 -0400

    AWS_ITER2

--- src/aws/aws_scan_client.py
+++ src/aws/aws_scan_client.py
@@ -6,23 +6,28 @@ from scapy.all import DNSQR
 from scapy.all import IP
 from scapy.all import UDP
 from scapy.all import send
+import ip_scan_packet as self_dns
 
 host = '18.188.87.197'
 port = 53
 
 if __name__ == '__main__':
-    #out_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM,socket.IPPROTO_UDP);
+    out_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM,socket.IPPROTO_UDP);
 
-    out_packet = IP(dst=host)/UDP()/DNS(qd=DNSQR(qname='test.dns',qtype="A"));
+    out_packet = self_dns.make_dns_packet("test.dns");
 
     start = time.time();
 
     count = 0;
-    while True:
-        send(out_packet,count=1024,verbose=False);
-        count = count + 1;
-        now = time.time();
-        if((now-start)>1):
-            start = now;
-            print("Packet sent in last second: ",count);
-            count = 0;
+    try:
+        while True:
+            out_socket.sendto(out_packet,(host,port));
+            count = count + 1;
+            now = time.time();
+            if((now-start)>1):
+                start = now;
+                print("Packet sent in last second: ",count);
+                count = 0;
+    except Exception as e:
+        print(e);
+        out_socket.close();
--- src/aws/aws_scan_server.py
+++ src/aws/aws_scan_server.py
@@ -2,6 +2,11 @@ import sys
 import socket
 import time
 import scapy
+from scapy.all import DNS
+from scapy.all import DNSQR
+from scapy.all import IP
+from scapy.all import UDP
+from scapy.all import send
 import ip_scan_packet as self_dns
 
 host = ''
@@ -15,15 +20,20 @@ if __name__ == '__main__':
     start = time.time();
 
     count = 0;
-    while True:
-        data = in_socket.recv(1024);
-        dns_packet = DNS(data);
-        qname = dns_packet[DNSQR].qname.decode();
+    try:
+        while True:
+            data = in_socket.recv(1024);
+            dns_packet = DNS(data);
+            qname = dns_packet[DNSQR].qname.decode();
+            #print(qname);
 
-        if(qname == 'test.dns'):
-            count = count + 1;
-            now = time.time();
-            if((now-start)>1):
-                start = now;
-                print("Packet rcvd in last second: ",count);
-                count = 0;
+            if(qname == 'test.dns.'):
+                count = count + 1;
+                now = time.time();
+                if((now-start)>1):
+                    start = now;
+                    print("Packet rcvd in last second: ",count);
+                    count = 0;
+    except Exception as e:
+        print(e);
+        in_socket.close();
